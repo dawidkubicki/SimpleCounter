05f03cba39224830ce716e3a2500be3ec1393d50
Dawid Kubicki <dawid@pluscode.io>
2020-11-30 22:08:44 +0100

    Initial Commit

---
+++ SimpleCounterSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleCounterSwiftUI
+//
+//  Created by Dawid Kubicki on 30/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleCounterSwiftUI/SimpleCounterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCounterSwiftUIApp.swift
+//  SimpleCounterSwiftUI
+//
+//  Created by Dawid Kubicki on 30/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
